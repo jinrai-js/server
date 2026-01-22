1dd13028d6e12d60c9fd8af1565a3a8a65e79f79
II <no_soap_0@mail.ru>
2026-01-22 09:03:35 +0300

    add extensions

---
+++ internal/extensions/extensions.go
@@ -0,0 +1,75 @@
+package extensions
+
+var Popular = []string{
+	// Текстовые документы
+	".txt", ".doc", ".docx", ".pdf", ".rtf", ".odt", ".tex", ".pages",
+
+	// Электронные таблицы
+	".xls", ".xlsx", ".csv", ".ods", ".numbers",
+
+	// Презентации
+	".ppt", ".pptx", ".odp", ".key",
+
+	// Архивы
+	".zip", ".rar", ".7z", ".tar", ".gz", ".bz2", ".xz", ".tgz",
+
+	// Исполняемые файлы
+	".exe", ".msi", ".app", ".apk", ".bat", ".sh", ".cmd", ".jar",
+
+	// Системные
+	".dll", ".sys", ".ini", ".cfg", ".log", ".tmp", ".bak",
+
+	// Изображения
+	".jpg", ".jpeg", ".png", ".gif", ".bmp", ".svg", ".webp", ".tiff", ".ico",
+	".psd", ".ai", ".eps", ".raw", ".heic",
+
+	// Видео
+	".mp4", ".avi", ".mov", ".mkv", ".wmv", ".flv", ".webm", ".mpeg", ".mpg",
+	".3gp", ".m4v", ".vob",
+
+	// Аудио
+	".mp3", ".wav", ".flac", ".aac", ".ogg", ".wma", ".m4a", ".opus",
+
+	// Веб
+	".html", ".htm", ".css", ".js", ".php", ".xml", ".json", ".yaml", ".yml",
+
+	// Код
+	".go", ".py", ".java", ".cpp", ".c", ".h", ".cs", ".rb", ".php", ".swift",
+	".kt", ".rs", ".ts", ".js", ".sql", ".pl", ".lua", ".m", ".r",
+
+	// Базы данных
+	".db", ".sqlite", ".sqlite3", ".mdb", ".accdb", ".dbf", ".mdf",
+
+	// Виртуальные машины и образы
+	".iso", ".vhd", ".vdi", ".vmx", ".img",
+
+	// Шрифты
+	".ttf", ".otf", ".woff", ".woff2", ".eot",
+
+	// Электронные книги
+	".epub", ".mobi", ".azw", ".fb2",
+
+	// Проекты
+	".sln", ".vcxproj", ".xcodeproj", ".pro", ".mk",
+
+	// Конфигурации
+	".env", ".toml", ".properties", ".conf", ".config",
+
+	// Графика и дизайн
+	".sketch", ".fig", ".xd", ".blend", ".max", ".mb", ".ma",
+
+	// Документация
+	".md", ".markdown", ".rst", ".tex", ".org",
+
+	// Субтитры
+	".srt", ".sub", ".ass", ".vtt",
+
+	// Шаблоны
+	".template", ".tpl", ".mustache", ".hbs",
+
+	// Сертификаты и ключи
+	".pem", ".crt", ".key", ".cer", ".pfx", ".p12",
+
+	// Другие популярные
+	".torrent", ".ics", ".vcf", ".contact",
+}
